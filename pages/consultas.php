e28d11a027c163c78e946e9d3177f3cb4c725022
Marcos Bah <marcos.ba@protonmail.com>
2019-04-28 19:28:36 -0300

    Sem alteração de codicos, apenas alterações infímas.

--- pages/consultas.php
+++ pages/consultas.php
@@ -14,7 +14,6 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/1.3.5/jspdf.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.min.js"></script>
 
-
 	<!-- INCLUDE funcoes ifsuldeminas -->
 	<script src="../js/funcoes.js"></script>
 	<script src="../js/gerarCharts_font.js"></script>
