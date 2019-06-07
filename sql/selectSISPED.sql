4e34bc51095624300c5c71a1f982623d4580c845
pedrinluiz <50276387+pedrinluiz@users.noreply.github.com>
2019-06-06 21:23:53 -0300

    Add files via upload

--- sql/selectSISPED.sql
+++ sql/selectSISPED.sql
@@ -1,16 +1,16 @@
-select instituicao.nome, instituicao.endereco, instituicao.cnpj, 
-dadosconsulta.perimetroCefalico, dadosconsulta.peso, dadosconsulta.altura, 
+select instituicao.nome, instituicao.endereco, instituicao.cnpj,
+dadosconsulta.perimetroCefalico, dadosconsulta.peso, dadosconsulta.altura,
 dadosconsulta.dataConsulta, dadosauxiliar.crm, dadosauxiliar.cpf, dadosauxiliar.nome
-from instituicao inner join dadosconsulta on instituicao.idinst = idInstituicao
+dadoscrianca.nome, dadoscrianca.nascimento, dadoscrianca.prematuro,
+dadoscrianca.diasPrematuro, dadoscrianca.sexo, dadosresponsavel.cpf, dadosresponsavel.nome
+from instituicao
+inner join dadosconsulta on instituicao.idinst = dadosconsulta.idInstituicao
+inner join dadoscrianca on dadosconsulta.idCrianca = dadoscrianca.idcrian
+inner join dadosresponsavel on dadoscrianca.idcrian = dadosresponsavel.idCrianca
 inner join dadosauxiliar on dadosconsulta.idAuxiliar = dadosauxiliar.idaux;
 
-select dadoscrianca.nome, dadoscrianca.nascimento, dadoscrianca.prematuro,
-dadoscrianca.diasPrematuro, dadoscrianca.sexo, dadosresponsavel.cpf, dadosresponsavel.nome
-from dadoscrianca inner join dadosresponsavel
-on dadoscrianca.idcrian = dadosresponsavel.idCrianca;
-
-select * from instituicao as i 
-INNER JOIN dadosconsulta as dc on i.idinst=dc.idInstituicao 
-INNER JOIN dadoscrianca as dcr ON dc.idCrianca=dcr.idcrian 
-INNER JOIN dadosresponsavel as dr ON dcr.idcrian=dr.idCrianca 
-INNER JOIN dadosauxiliar as da ON dc.idAuxiliar=da.idaux 
+select * from instituicao as i
+INNER JOIN dadosconsulta as dc on i.idinst=dc.idInstituicao
+INNER JOIN dadoscrianca as dcr ON dc.idCrianca=dcr.idcrian
+INNER JOIN dadosresponsavel as dr ON dcr.idcrian=dr.idCrianca
+INNER JOIN dadosauxiliar as da ON dc.idAuxiliar=da.idaux
