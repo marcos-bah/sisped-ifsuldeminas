2503b0a5b1d9c1fec0579389ef772b06cdcd5764
Marcos Bah <marcos.ba@protonmail.com>
2019-09-01 15:22:11 -0300

    Update sql

--- sql/sisped.sql
+++ sql/sisped.sql
@@ -38,14 +38,9 @@ create table dadosresponsavel(
 create table dadosauxiliar(
   idaux int not null auto_increment primary key,
   crm varchar(10),
-<<<<<<< HEAD
-  nome varchar(255) not null,
-  cpf varchar(11) not null
-=======
   cpf varchar(11) not null,
   iduser int not null,
   constraint fk_AuxiliarUser foreign key(iduser) references sispeduser(iduse)
->>>>>>> 5f5e8e24dce5b8b41df94e5ce3bf76302edaeb84
 );
 
 create table dadosconsulta(
@@ -63,20 +58,14 @@ create table dadosconsulta(
   constraint fk_InstituicaoConsulta foreign key (idInstituicao) references instituicao(idinst)
 );
 
-<<<<<<< HEAD
-create table sispeduser(
-  iduse int not null primary key auto_increment,
-  nameuser varchar(100),
-  password varchar(255)
-);
 
-ALTER TABLE `dadosconsulta` ADD `obs` VARCHAR(255) NOT NULL AFTER `altura`;
+
 
 INSERT INTO `instituicao` (`idinst`, `nome`, `endereco`, `cnpj`, `ativo`) VALUES (NULL, 'APAE', 'Rua XV de Novembro', '4243234-322', '1');
 INSERT INTO `dadosauxiliar` (`idaux`, `crm`, `nome`, `cpf`) VALUES (NULL, '2423-MG', 'Usuario Teste', '842349328');
-=======
+
 INSERT INTO `instituicao` (`idinst`, `nome`, `endereco`, `cnpj`, `ativo`) VALUES (NULL, 'APAE', 'Rua XV de Novembro', '4243234-322', '1');
 INSERT INTO `dadosauxiliar` (`idaux`, `crm`, `nome`, `cpf`) VALUES (NULL, '2423-MG', 'Médico Atual', '842349328');
 INSERT INTO `dadoscrianca` (`idcrian`, `nome`, `nascimento`, `prematuro`, `diasPrematuro`, `sexo`) VALUES (NULL, 'Crianca Zero', '2010-02-02', false, 0, 'm');
 INSERT INTO `sispeduser` (`iduse`, `nameuser`, `password`) VALUES (NULL, 'sisped', 'd32129481a7f1fc4cb052f698e8792ca96477fc1');
->>>>>>> 5f5e8e24dce5b8b41df94e5ce3bf76302edaeb84
+
